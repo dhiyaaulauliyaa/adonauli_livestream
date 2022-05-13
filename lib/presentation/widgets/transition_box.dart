ca7b0c0f70a206a4a1fc98b5b667a074858b71b6
Dhiyaaul Auliyaa <ahmad.auliyaa@stockbit.com>
2022-05-13 13:43:46 +0700

    Widget | Experiment on TransitionBox

--- lib/presentation/widgets/transition_box.dart
+++ lib/presentation/widgets/transition_box.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:show_up_animation/show_up_animation.dart';
 
 import 'hidden_widget.dart';
@@ -10,13 +11,13 @@ class TransitionBox extends StatelessWidget {
     this.show = true,
     this.startDelay = Duration.zero,
     this.duration = const Duration(milliseconds: 2000),
-    this.direction = Direction.vertical,
+    this.direction = Direction.horizontal,
     this.curve = Curves.easeOutCirc,
-    this.offset = 1,
+    this.offset = -1,
   }) : super(key: key);
 
-  final Widget child;
   final bool show;
+  final Widget child;
   final Duration startDelay;
   final Duration duration;
   final Direction direction;
@@ -25,20 +26,44 @@ class TransitionBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedSwitcher(
-      duration: Duration(milliseconds: show ? 200 : 0),
-      child: show
-          ? ShowUpAnimation(
-              animationDuration: duration,
-              curve: curve,
-              delayStart: startDelay,
-              direction: direction,
-              offset: offset,
-              child: child,
-            )
-          : HiddenWidget(
-              child: child,
-            ),
-    );
+    if (show) {
+      return _buildWidget();
+    } else {
+      return HiddenWidget(child: child);
+    }
+    // return AnimatedSwitcher(
+    //   duration: Duration(milliseconds: show ? 200 : 0),
+    //   child: show
+    //       ? newMethod()
+    //       : HiddenWidget(
+    //           child: child,
+    //         ),
+    // );
   }
+
+  Widget _buildWidget() {
+    return ShowUpAnimation(
+      animationDuration: duration,
+      curve: curve,
+      delayStart: startDelay,
+      direction: direction,
+      offset: offset,
+      child: child,
+    );
+    // return AnimatedContainer(
+    //   duration: duration,
+    //   transform: getTransform(show),
+    //   child: child,
+    // );
+  }
+
+  // getTransform(bool show) {
+  //   if (show) {
+  //     return Matrix4.identity();
+  //   } else {
+  //     return Matrix4.identity()
+  //       ..translate(-1.sw, 0)
+  //       ..scale(1, 1);
+  //   }
+  // }
 }
