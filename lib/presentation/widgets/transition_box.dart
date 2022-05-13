bcfd0921ca21576aec9694e0f441e56f461c26ae
Dhiyaaul Auliyaa <ahmad.auliyaa@stockbit.com>
2022-05-13 13:10:29 +0700

    Widget | Create TransitionBox

---
+++ lib/presentation/widgets/transition_box.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:show_up_animation/show_up_animation.dart';
+
+import 'hidden_widget.dart';
+
+class TransitionBox extends StatelessWidget {
+  const TransitionBox({
+    Key? key,
+    required this.child,
+    this.show = true,
+    this.startDelay = Duration.zero,
+    this.duration = const Duration(milliseconds: 2000),
+    this.direction = Direction.vertical,
+    this.curve = Curves.easeOutCirc,
+    this.offset = 1,
+  }) : super(key: key);
+
+  final Widget child;
+  final bool show;
+  final Duration startDelay;
+  final Duration duration;
+  final Direction direction;
+  final Curve curve;
+  final double offset;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSwitcher(
+      duration: Duration(milliseconds: show ? 200 : 0),
+      child: show
+          ? ShowUpAnimation(
+              animationDuration: duration,
+              curve: curve,
+              delayStart: startDelay,
+              direction: direction,
+              offset: offset,
+              child: child,
+            )
+          : HiddenWidget(
+              child: child,
+            ),
+    );
+  }
+}
